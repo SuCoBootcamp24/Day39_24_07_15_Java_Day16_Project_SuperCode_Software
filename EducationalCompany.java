383c488cb9d54fe4830a393ecc1402b0df4197b9
René Behrens <rene.behrens@aol.com>
2024-07-15 15:25:01 +0200

    update EducationalCompany.claas

--- EducationalCompany.java
+++ EducationalCompany.java
@@ -8,12 +8,121 @@ public class EducationalCompany {
     private String category;
     private ArrayList<Trainer> trainerList = new ArrayList<>();
     private ArrayList<Student> studentList = new ArrayList<>();
-    private ArrayList<Cours> coursList = new ArrayList<>();
+    private ArrayList<Course> courseList = new ArrayList<>();
 
 
     public EducationalCompany(Ceo ceo, LocalDate foundingDate, String category) {
-        
+
     }
     
+    //-----Getter / Setter-----
+    public Ceo getCeo() {
+        return ceo;
+    }
+
+    public void setCeo(Ceo ceo) {
+        this.ceo = ceo;
+    }
+
+    //---
+    public LocalDate getFoundingDate() {
+        return foundingDate;
+    }
+
+    public void setFoundingDate(LocalDate foundingDate) {
+        this.foundingDate = foundingDate;
+    }
+
+    //---
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     
+    //---
+    public ArrayList<Trainer> getTrainerList() {
+        return trainerList;
+    }
+
+    public void setTrainerList(ArrayList<Trainer> trainerList) {
+        this.trainerList = trainerList;
+    }
+
+   //---
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+   //---
+    public ArrayList<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(ArrayList<Course> courseList) {
+        this.courseList = courseList;
+    }
+
+    //------other
+
+
+    public void changeCeo(Ceo ceo) {
+
+    }
+
+    public void addTrainer(Trainer trainer) {
+
+    }
+
+    public void removeTrainer(Trainer trainer) {
+
+    }
+
+    public void addStudent(Student student) {
+
+    }
+
+    public void removeStudent(Student student) {
+
+    }
+
+    public void addCourse(Course course) {
+
+    }
+
+    public void printTrainer() {
+
+    }
+
+    public void printCourse() {
+
+    }
+
+    @Override
+    public String toString(){
+        return null;
+    }
+
+    public void addTrainerToCourse(Course course) {
+
+    }
+
+
+    public int CaddStudentToCourse(Course course) {
+        return 0;
+    }
+
+    public void removeCourse(Course course){
+
+    }
+
+
+
 }
