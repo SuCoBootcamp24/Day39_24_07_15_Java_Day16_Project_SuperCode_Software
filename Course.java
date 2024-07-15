a78b6aa940a2c01f30d687de4496b4dcb7d537c5
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-07-15 16:10:28 +0200

    add Student

---
+++ Course.java
@@ -0,0 +1,55 @@
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public class Course {
+    private static long lastID = 0;
+    private long ID;
+    private String name;
+    private LocalDate startingDate;
+    private LocalDate endingDate;
+    private ArrayList<Module> modules;
+    private ArrayList<Student> students;
+    private final int MAX_STUDENT = 8;
+
+    public Course(String name, LocalDate startingDate, LocalDate endingDate) {
+        this.name = name;
+        this.startingDate = startingDate;
+        this.endingDate = endingDate;
+        this.ID = lastID + 1;
+        Course.lastID++;
+        this.modules = new ArrayList<>();
+        this.students = new ArrayList<>();
+    }
+    
+    public long getID() {
+        return ID;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public LocalDate getStartingDate() {
+        return startingDate;
+    }
+    
+    public LocalDate getEndingDate() {
+        return endingDate;
+    }
+    
+    public ArrayList<Module> getModules() {
+        return modules;
+    }
+    
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+    
+    public int getMAX_STUDENT() {
+        return MAX_STUDENT;
+    }
+
+
+}
---
+++ Student.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+
+public class Student extends Person {
+    private Course hisCourse;
+    private String desiredCourse;
+
+    public Student(String lastname, String firstname, LocalDate birthday, String address, String email, String desiredCourse) {
+        super(lastname, firstname, birthday, address, email);
+        this.desiredCourse = desiredCourse;
+        this.hisCourse = new Course();
+    }
+    
+    public void setCourse(Course course) {
+        this.hisCourse = course;
+    }
+    
+}
