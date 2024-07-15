0784ba1bdc9337454c70794dd9d12530e65c8707
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-07-15 15:12:22 +0200

    modify lastID Person class

---
+++ Trainer.java
@@ -0,0 +1,11 @@
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Trainer extends Person {
+    private ArrayList<String> licenses;
+    private LocalDate inCompanySice;
+
+    public Trainer ()
+    
+}
