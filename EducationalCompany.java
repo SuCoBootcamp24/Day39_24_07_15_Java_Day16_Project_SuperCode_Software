ec41579b02b7e51676257fba425a435cb9b5e064
René Behrens <rene.behrens@aol.com>
2024-07-15 15:12:21 +0200

    add EducationalCompany

---
+++ EducationalCompany.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class EducationalCompany {
+
+    private Ceo ceo;
+    private LocalDate foundingDate;
+    private String category;
+    private ArrayList<Trainer> trainerList = new ArrayList<>();
+    private ArrayList<Student> studentList = new ArrayList<>();
+    private ArrayList<Cours> coursList = new ArrayList<>();
+
+
+    public EducationalCompany(Ceo ceo, LocalDate foundingDate, String category) {
+        
+    }
+    
+    
+}
