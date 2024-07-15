6d706957f47287fbd99a87b4a6eeea204010a7e9
René Behrens <rene.behrens@aol.com>
2024-07-15 15:40:32 +0200

    add Ceo.claas

--- Ceo.java
+++ Ceo.java
@@ -1,3 +1,37 @@
-public class Ceo {
-    
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Ceo extends Person{
+
+    private ArrayList<EducationalCompany> campanyList = new ArrayList<>();
+    private LocalDate inCompany;
+
+    public Ceo(String lastname, String firstname, LocalDate birthday, String address, String email, LocalDate inCompany, EducationalCompany educationalCompany) {
+        super(lastname, firstname, birthday, address, email);
+        addCampany(educationalCompany);
+        setInCompany(inCompany);
+
+    }
+
+
+    public ArrayList<EducationalCompany> getCampanyList() {
+        return campanyList;
+    }
+
+    public void addCampany(EducationalCompany company) {
+
+        if (this.campanyList.contains(company))this.campanyList.add(company);
+    }
+
+
+    public LocalDate getInCompany() {
+        return inCompany;
+    }
+
+    public void setInCompany(LocalDate inCompany) {
+        this.inCompany = inCompany;
+    }
+
 }
