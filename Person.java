9fab612a34b926bdb4b60dc7bd094a02d715915d
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-07-15 15:08:55 +0200

    add Person class

---
+++ Person.java
@@ -0,0 +1,60 @@
+import java.time.LocalDate;
+
+public class Person {
+
+    public static long getLastID() {
+        return lastID;
+    }
+    private String lastname;
+    private String firstname;
+    private static long lastID;
+    private long ID;
+    private String address;
+    private String email;
+    private LocalDate birthday;
+
+    public Person(String lastname, String firstname, LocalDate birthday, String address, String email) {
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.birthday = birthday;
+        this.ID = lastID + 1;
+        Person.lastID++;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Student " + this.ID + ": " +
+                "DOB: " + birthday + 
+                "Address: " + address + 
+                "Email: " + email;
+    }
+
+    
+
+}
