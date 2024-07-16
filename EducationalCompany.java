a254c1188b57b989c41d7b8c5aff33a50bbac25d
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-07-16 16:02:11 +0200

    .

--- EducationalCompany.java
+++ EducationalCompany.java
@@ -86,73 +86,51 @@ public class EducationalCompany {
     //------other
 
     public void addTrainer(Trainer trainer) {
-        if (!getTrainerList().contains(trainer)) getTrainerList().add(trainer);
+
     }
 
     public void removeTrainer(Trainer trainer) {
-        if (getTrainerList().contains(trainer)) getTrainerList().remove(trainer);
+
     }
 
     public void addStudent(Student student) {
-        if (!getStudentList().contains(student)) getStudentList().add(student);
+
     }
 
     public void removeStudent(Student student) {
-        if (getStudentList().contains(student)) getStudentList().add(student);
+
     }
 
     public void addCourse(Course course) {
-        if (!getCourseList().contains(course)) getCourseList().add(course);
-    }
 
-    public void removeCourse(Course course) {
-        if (getCourseList().contains(course)) getCourseList().add(course);
     }
 
     public void printTrainer() {
-        System.out.println("List of Trainers for " + getName() + ":");
-        if(trainerList == null) System.out.println(" Trainers not added");
-        else {
-            for (Trainer t : trainerList) {
-                System.out.println("\t" + t);
-            }
-        }
+
     }
 
     public void printCourse() {
-        System.out.println("List of Courses for " + getName() + ":");
-        if(courseList == null) System.out.println(" Courses not added");
-        else {
-            for (Course c : courseList) {
-                System.out.println("\t" + c);
-            }
-        }
+
     }
 
     @Override
     public String toString(){
-        return getName() + ", Category: " + getCategory() + ", Founding Date: " + getFoundingDate();
+        return null;
     }
 
     public void addTrainerToCourse(Course course) {
-        for (int i = 0; i < course.getModules().size(); i++) {
-            Module courseModule = course.getModules().get(i);
-            if (courseModule.getTrainer() != null) {
-                for (Trainer t : trainerList) {
-                    if (t.hasLicense(courseModule)) {
-                        if (t.TrainerIsFree(courseModule)) {
-                            courseModule.setTrainer(t);
-                            System.out.println("added " + courseModule.getName());
-                        } else System.out.println("Zeit passt nicht " + courseModule.getName());
-                    } else System.out.println("keine lizenz " + courseModule.getName());
-                }
-            }
-        }
+
     }
 
 
     public int CaddStudentToCourse(Course course) {
         return 0;
     }
+
+    public void removeCourse(Course course){
+
+    }
+
+
 
 }
